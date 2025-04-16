943367df10b379db68509d6beb505c0746248ac2
Nelson Zaraza <nelsonmauricionavarrozaraza@gmail.com>
2025-04-16 00:28:00 -0500

    feat: add login views for employee and children using Tailwind CSS
    Created the static UI for the employee and children login screens using HTML and Tailwind CSS. No functionality or logic implemented yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import LoginEmployee from "./components/loginEmployee/loginEmployee";
+import LoginChildren from "./components/loginChildren/LoginChildren";
 
 export default function App() {
   return (
-    <></>
-  )
+    <>
+      <LoginChildren />
+    </>
+  );
 }
